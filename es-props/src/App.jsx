e26de3724e8df54f6c9d2c00f8deba9dca084649
GabrieleGiann <gabrielegiannattasio20@gmail.com>
2025-05-13 09:30:32 +0200

    esercizio props

---
+++ es-props/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Welcome from './components/Welcome'
+
+
+function App() {
+
+  return (
+    <>
+      <Welcome name={"pippo"} age={18}/>
+    </>
+  )
+}
+
+export default App
---
+++ es-props/src/components/Age.jsx
@@ -0,0 +1,9 @@
+
+
+function Age(props){
+  return (
+    <div>Your age is {props.age}</div>
+  )
+}
+
+export default Age
